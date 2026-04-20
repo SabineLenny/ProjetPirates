204f285455490074046c3ad267a8b5d45e074359
Dapoce <104147056+Dapoce@users.noreply.github.com>
2026-04-20 11:06:35 +0200

    feat : premier push

---
+++ Code/ProjetPirate/src/main/java/com/mycompany/projetpirate/ProjetPirate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetpirate;
+
+/**
+ *
+ * @author chass
+ */
+public class ProjetPirate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
