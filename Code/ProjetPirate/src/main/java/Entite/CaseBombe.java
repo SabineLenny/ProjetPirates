3ab984eb3fb926c9b512bb2ad2a53ae16478c7f0
elouanpauna <elouan.tilou@hotmail.fr>
2026-04-24 16:57:46 +0200

    feats: correction de code Cases Bombe et poison

--- Code/ProjetPirate/src/main/java/Entite/CaseBombe.java
+++ Code/ProjetPirate/src/main/java/Entite/CaseBombe.java
@@ -6,8 +6,7 @@ public class CaseBombe implements Case {
 	//Elouan
 	@Override
 	public String appliquerEffet(Pirate pirateActif,Pirate autrePirate) {
-		ControlLancerDeDés lancerDes = new ControlLancerDeDés();
-		int i[]= lancerDes.lancerDés(1);
+		int i[]= ControlLancerDeDés.lancerDés(1);
 		pirateActif.setVie(i[0]);
 		String result="Le pirate perd " + Integer.toString(i[0]) + "points de vie\n";
 		return result;
--- Code/ProjetPirate/src/main/java/Entite/CasePoison.java
+++ Code/ProjetPirate/src/main/java/Entite/CasePoison.java
@@ -6,8 +6,7 @@ public class CasePoison implements Case {
 	//Elouan
 	@Override
 	public String appliquerEffet(Pirate pirateActif,Pirate autrePirate) {
-		ControlLancerDeDés lancerDes = new ControlLancerDeDés();
-		int i[]= lancerDes.lancerDés(1);
+		int i[]= ControlLancerDeDés.lancerDés(1);
 		pirateActif.setDuréeEmpoisonnement(i[0]);
 		String result="Le pirate est empoisonné pour " + Integer.toString(i[0]) + "tours!!!\n";
 		return result;
