1bd879a47ef76aa3b7a9659cd358c197bcc22bd8
Dapoce <104147056+Dapoce@users.noreply.github.com>
2026-05-13 09:36:47 +0200

    Reapply "fix:retrait accents dans control dés"
    This reverts commit f997cc0aa4f302454d5a85e14e67eff6c1e82b06.

--- Code/src/main/java/Controleurs/ControlCaseBombe.java
+++ Code/src/main/java/Controleurs/ControlCaseBombe.java
@@ -4,10 +4,10 @@ import Entite.Pirate;
 
 public class ControlCaseBombe {
     
-    private final ControlLancerDeDés controlDés = new ControlLancerDeDés();
+    private final ControlLancerDeDes controlDés = new ControlLancerDeDes();
     
     public String appliquerEffet(Pirate pirate){
-        int[] resultat = controlDés.lancerDés(1);
+        int[] resultat = controlDés.lancerDes(1);
         int degats = resultat[0]/2;
         if (degats == 0) degats = 1;
         pirate.prendreDegats(degats);
