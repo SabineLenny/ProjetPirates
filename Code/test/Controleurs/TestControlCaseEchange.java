61bba4e16d8b6aa209cf0f3dc538f437897e1cd6
Ley0s <lucas.gorgues31270@gmail.com>
2026-05-20 16:43:21 +0200

    feat: tests (par Maxim, corriges par Lucas)

---
+++ Code/test/Controleurs/TestControlCaseEchange.java
@@ -0,0 +1,37 @@
+package Controleurs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Entite.Pirate;
+
+class TestControlCaseEchange {
+    
+    private Pirate pirate1;
+    private Pirate pirate2;
+    private ControlCaseEchange controlCaseEchange;
+    
+    @BeforeEach
+    void setUp() {
+        pirate1 = new Pirate("Pirate1");
+        pirate2 = new Pirate("Pirate2");
+        pirate1.setPosition(10);
+        pirate2.setPosition(20);
+    }
+    
+    @Test
+    void testAppliquerEffetEchangePositions() {
+        controlCaseEchange.appliquerEffet(pirate1, pirate2);
+        assertEquals(20, pirate1.getPosition());
+        assertEquals(10, pirate2.getPosition());
+    }
+    
+    @Test
+    void testAppliquerEffetEchangePosition0() {
+        pirate1.setPosition(0);
+        pirate2.setPosition(15);
+        controlCaseEchange.appliquerEffet(pirate1, pirate2);
+        assertEquals(15, pirate1.getPosition());
+        assertEquals(0, pirate2.getPosition());
+    }
+}
