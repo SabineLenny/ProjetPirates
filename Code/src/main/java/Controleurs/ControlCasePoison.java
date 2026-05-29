1ca71c5a86fcae5330043e62cfb0fbc9faa29906
Maksym Vyshnyakov <maksymvyshnyakov@MacBook-Pro-de-Maksym.local>
2026-05-29 12:07:38 +0200

    fix: poison

--- Code/src/main/java/Controleurs/ControlCasePoison.java
+++ Code/src/main/java/Controleurs/ControlCasePoison.java
@@ -9,7 +9,6 @@ public class ControlCasePoison {
     public String appliquerEffet(Pirate pirate1, Pirate pirate2){
         int[] resultat = CONTROL_DES.lancerDes(1);
         pirate1.setDureeEmpoisonnement(resultat[0]);
-        pirate1.soigner();
         return pirate1.getNom() + " est empoisonne pendant " + resultat[0] + " tours.\n";
     }
 }
